e580d2cf8cc3893c14727cd9f31b9e973c8f8d1b
Michael Velasco <michael.velasco.amaya@gmail.com>
2023-02-01 08:55:18 -0500

    add responsive font sizes theme

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './internationalization/i18n';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@emotion/react';
+import theme from './others/themeApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <I18nextProvider i18n={i18n}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </I18nextProvider>
+  <ThemeProvider theme={theme}>
+    <I18nextProvider i18n={i18n}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </I18nextProvider>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
