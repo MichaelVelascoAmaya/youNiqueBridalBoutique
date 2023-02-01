70d5e130d0047c7cfa22fc0e708e38af816fe694
Michael Velasco <michael.velasco.amaya@gmail.com>
2023-01-31 19:30:09 -0500

    base structure

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './App.module.css';
+
+const App = () => (
+  <div className={styles.App} data-testid="App">
+    App Component
+  </div>
+);
+
+App.propTypes = {};
+
+App.defaultProps = {};
+
+export default App;
