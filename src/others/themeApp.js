6627ddae4dc7416a1d9a100d1f2e84bf021f9cca
Michael Velasco <michael.velasco.amaya@gmail.com>
2023-02-16 17:48:28 -0500

    add theme palette colors

--- src/others/themeApp.js
+++ src/others/themeApp.js
@@ -1,5 +1,20 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
-const theme = responsiveFontSizes(createTheme({}));
+const theme = responsiveFontSizes(createTheme({
+  palette: {
+    primary: {
+      main: '#f05696',
+    },
+    secondary: {
+      main: '#917944',
+    },
+    white: {
+      main: '#FFFFFF',
+    },
+    black: {
+      main: '#000000',
+    },
+  },
+}));
 
 export default theme;
