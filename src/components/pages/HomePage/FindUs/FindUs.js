d269b7eff9aae8b41094f75db10eaa1369b0aa22
Michael Velasco <michael.velasco.amaya@gmail.com>
2023-02-19 12:32:43 -0500

    add FindUs component info

--- src/components/pages/HomePage/FindUs/FindUs.js
+++ src/components/pages/HomePage/FindUs/FindUs.js
@@ -1,15 +1,103 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FindUs.module.css';
+import { Box, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
+import { withTranslation } from 'react-i18next';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import DateRangeOutlinedIcon from '@mui/icons-material/DateRangeOutlined';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import AttachEmailOutlinedIcon from '@mui/icons-material/AttachEmailOutlined';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 
-const FindUs = () => (
-  <div className={styles.FindUs} data-testid="FindUs">
-    FindUs Component
-  </div>
+const FindUs = ({ t }) => (
+  <Box className={styles.FindUs} data-testid="FindUs">
+    <Grid container
+      direction="row"
+      justifyContent="space-around"
+      alignItems="center"
+    >
+      <Grid item xs={12} sm={6} >
+        <Box className={styles['grid-section']} >
+          <List dense={false}>
+
+            <ListItem>
+              <ListItemIcon> <CalendarMonthOutlinedIcon className={styles.icon} /> </ListItemIcon>
+              <ListItemText
+                primaryTypographyProps={{ className: styles.text }}
+                primary={`${t('weekDays.monday')} - ${t('weekDays.friday')}`}
+                secondary={`${t('withoutTranslations.schedules.mondayToFriday')}`}
+              />
+            </ListItem>
+
+            <ListItem>
+              <ListItemIcon> <DateRangeOutlinedIcon className={styles.icon} /> </ListItemIcon>
+              <ListItemText
+                primaryTypographyProps={{ className: styles.text }}
+                primary={`${t('weekDays.saturday')}`}
+                secondary={`${t('withoutTranslations.schedules.saturday')}`}
+              />
+            </ListItem>
+
+          </List>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6} >
+        <Box className={styles['grid-section']} >
+          <List dense={false}>
+
+            <ListItem>
+              <ListItemIcon> <LocalPhoneOutlinedIcon className={styles.icon} /> </ListItemIcon>
+              <ListItemText
+                primaryTypographyProps={{ 
+                  component: 'a', 
+                  href:`tel:${t('withoutTranslations.info.phones.phoneOne.indicative')}${t('withoutTranslations.info.phones.phoneOne.number')}`, 
+                  className: styles.text 
+                }}
+                secondaryTypographyProps={{ 
+                  component: 'a', 
+                  href:`tel:${t('withoutTranslations.info.phones.phoneTwo.indicative')}${t('withoutTranslations.info.phones.phoneTwo.number')}`, 
+                  className: styles.text 
+                }}
+                primary={`${t('withoutTranslations.info.phones.phoneOne.formated')}`}
+                secondary={`${t('withoutTranslations.info.phones.phoneTwo.formated')}`}
+              />
+            </ListItem>
+
+            <ListItem>
+              <ListItemIcon> <AttachEmailOutlinedIcon className={styles.icon} /> </ListItemIcon>
+              <ListItemText
+                primaryTypographyProps={{ 
+                  component: 'a',
+                  href: `mailto:${t('withoutTranslations.info.email')}`,
+                  className: styles.text 
+                }}
+                primary={`${t('withoutTranslations.info.email')}`}
+              />
+            </ListItem>
+
+            <ListItem>
+              <ListItemIcon> <LocationOnOutlinedIcon className={styles.icon} /> </ListItemIcon>
+              <ListItemText
+                primaryTypographyProps={{ 
+                  component: 'a',
+                  href: `${t('withoutTranslations.info.location.googleMaps')}`,
+                  target:'_blank',
+                  className: styles.text 
+                }}
+                primary={`${t('withoutTranslations.info.location.formated')}`}
+              />
+            </ListItem>
+
+          </List>
+        </Box>
+      </Grid>
+    </Grid>
+  </Box>
 );
 
 FindUs.propTypes = {};
 
 FindUs.defaultProps = {};
 
-export default FindUs;
+export default withTranslation()(FindUs);
--- src/internationalization/languages/withoutTranslation.js
+++ src/internationalization/languages/withoutTranslation.js
@@ -10,6 +10,29 @@ const withoutTranslations = {
       url: 'https://www.instagram.com/younique_bridal',
     },
   },
+  schedules: {
+    mondayToFriday: '12:00 am. - 07:00 pm.',
+    saturday: '11:00 am. - 06:00 pm.',
+  },
+  info: {
+    phones: {
+      phoneOne: {
+        indicative: '+1',
+        number: 7864706868,
+        formated: '+1 786-470-6868',
+      },
+      phoneTwo: {
+        indicative: '',
+        number: 3054545442,
+        formated: '(305) 454-5442',
+      },
+    },
+    email: 'info@youniquebridalboutique.com',
+    location: {
+      googleMaps: 'https://goo.gl/maps/tj4CZa2ufYHgzoJ39',
+      formated: '9999 SW 72 Street, suite 103 | Miami, Florida 33173'
+    },
+  }
 };
 
 export default withoutTranslations;
